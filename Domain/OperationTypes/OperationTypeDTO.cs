d4ecc1fdb6a4caa232330f1ba8ec404df96b2a01
David Ferreira da Silva Marques <1221276@isep.ipp.pt>
2024-10-18 15:01:20 +0100

    update OperationTypeDto

--- Domain/OperationTypes/OperationTypeDTO.cs
+++ Domain/OperationTypes/OperationTypeDTO.cs
@@ -2,5 +2,9 @@ namespace DDDSample1.Domain.OperationTypes {
 
     public class OperationTypeDTO {
 
+        public string id { get; set; }
+        public string name { get; set; }
+        public int estimatedDuration { get; set; }
+   
     }
 }
