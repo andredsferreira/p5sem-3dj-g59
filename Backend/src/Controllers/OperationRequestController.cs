14ac07578e34652f4a43334be590e573fac8005b
andredsferreira <andredsferreira2@gmail.com>
2024-10-26 15:11:31 +0100

    #16#17#18#19 Small update to controller

--- Backend/src/Controllers/OperationRequestController.cs
+++ Backend/src/Controllers/OperationRequestController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DDDSample1.Domain.Auth;
 using DDDSample1.Domain.OperationRequests;
@@ -23,28 +24,28 @@ public class OperationRequestController : ControllerBase {
     [Authorize(Roles = HospitalRoles.Doctor)]
     public async Task<ActionResult<OperationRequestDTO>> CreateOperationRequest([FromForm] OperationRequestDTO dto) {
         var createdOperationRequest = await _service.CreateOperationRequest(dto);
-        return Ok(createdOperationRequest);
+        return createdOperationRequest != null ? Ok(createdOperationRequest) : BadRequest("Could not create operation request");
     }
 
     [HttpPut("update")]
     [Authorize(Roles = HospitalRoles.Doctor)]
     public async Task<ActionResult<UpdatedOperationRequestDTO>> UpdateOperationRequest([FromForm] UpdatedOperationRequestDTO dto) {
         var updatedOperationRequest = await _service.UpdateOperationRequest(dto);
-        return CreatedAtAction("Updated operation request with ID: ", updatedOperationRequest.updatedId);
+        return updatedOperationRequest != null ? Ok(updatedOperationRequest) : BadRequest("Could not update operation request");
     }
 
     [HttpDelete("delete")]
     [Authorize(Roles = HospitalRoles.Doctor)]
     public async Task<ActionResult<Guid>> DeleteOperationRequest(Guid id) {
         var deletedRequestId = await _service.DeleteOperationRequest(id);
-        return CreatedAtAction("Deleted operation request with ID: ", deletedRequestId);
+        return deletedRequestId != null ? Ok(deletedRequestId) : BadRequest("Could not delete operation request");
     }
 
     [HttpGet("list")]
     [Authorize(Roles = HospitalRoles.Doctor)]
     public async Task<ActionResult<List<OperationRequestDTO>>> ListOperationRequests() {
         var operationRequests = await _service.ListOperationRequests();
-        return operationRequests;
+        return operationRequests.Any() ? Ok(operationRequests) : NotFound("No operation requests were found");
     }
 
 }
