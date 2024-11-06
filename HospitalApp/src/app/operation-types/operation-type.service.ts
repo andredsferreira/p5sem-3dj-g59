aa3783d4029d95c4e6c8b5e4f22df3131f75145c
David Ferreira da Silva Marques <1221276@isep.ipp.pt>
2024-11-06 15:56:14 +0000

    #46 added operation-type.service

---
+++ HospitalApp/src/app/operation-types/operation-type.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+//import { RequestStatus } from './request-status.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OperationTypeService{
+
+  private token = localStorage.getItem('token');
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  async createOperationType(name: string, anaesthesiaTime: number, surgeryTime: number, cleaningTime: number, 
+    status: string, specializations: string, minDoctor: number, minAnaesthetist: number, minInstrumentNurse: number,
+    minNurseAnaesthetist: number, minXRay: number, minMedicalAssistant: number): Promise<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`,
+      'Content-Type': 'application/json'
+    });
+    const body = {
+      name,
+      anaesthesiaTime,
+      surgeryTime,
+      cleaningTime,
+      status,
+      specializations,
+      minDoctor,
+      minAnaesthetist,
+      minInstrumentNurse,
+      minNurseAnaesthetist,
+      minXRay,
+      minMedicalAssistant
+    };
+    
+    try{
+      const response = await lastValueFrom(
+        this.http.post('https://localhost:5001/api/operationtype/Add',
+          body, { headers })
+      );
+      return response;
+    } catch (error) {
+      console.error('Error creating operation type:', error);
+      throw error;
+    }
+
+  }
+  
+
+
+}
