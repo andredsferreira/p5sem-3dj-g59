cb7ad18ac3687cd01e1ab72002e0d107c5d37d8d
andredsferreira <andredsferreira2@gmail.com>
2024-12-05 20:51:34 +0000

    #78 Create allergy method in service.

--- ExpressBackend/src/services/allergyService.ts
+++ ExpressBackend/src/services/allergyService.ts
@@ -6,16 +6,31 @@ import { Result } from "../core/logic/Result";
 import IAllergyService from "./IServices/IAllergyService";
 import IAllergyDTO from "../dto/IAllergyDTO";
 import IAllergyRepo from "./IRepos/IAllergyRepo";
+import { Allergy } from "../domain/allergy";
+import { AllergyMap } from "../mappers/AllergyMap";
 
 @Service()
 export default class AllergyService implements IAllergyService {
 
-    constructor(@Inject(config.repos.allergy.name) private AllergyRepo: IAllergyRepo) {
+    constructor(@Inject(config.repos.allergy.name) private allergyRepo: IAllergyRepo) {
 
     }
 
-    createAllergy(allergyDTO: IAllergyDTO): Promise<Result<IAllergyDTO>> {
-        throw new Error("Method not implemented.");
+    public async createAllergy(allergyDTO: IAllergyDTO): Promise<Result<IAllergyDTO>> {
+        try {
+            const allergyOrError = await Allergy.create(allergyDTO)
+            if (allergyOrError.isFailure) {
+                return Result.fail<IAllergyDTO>(allergyOrError.errorValue())
+            }
+
+            const allergyResult = allergyOrError.getValue()
+            await this.allergyRepo.save(allergyResult)
+
+            const allergyResultDTO = AllergyMap.toDTO(allergyResult) as IAllergyDTO
+            return Result.ok<IAllergyDTO>(allergyResultDTO)
+        } catch (err) {
+            throw err
+        }
     }
 
     getAllergyByName(name: string): Promise<Result<IAllergyDTO>> {
