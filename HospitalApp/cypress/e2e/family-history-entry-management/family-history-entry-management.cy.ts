c24d753b3aea220a55c00a306dd9e87952c02c2d
Diogo Cunha <1221071@isep.ipp.pt>
2024-12-30 15:35:24 +0000

    e2e tests added

--- HospitalApp/cypress/e2e/family-history-entry-management/family-history-entry-management.cy.ts
+++ HospitalApp/cypress/e2e/family-history-entry-management/family-history-entry-management.cy.ts
@@ -1,11 +1,11 @@
 let tokenFamilyHistoryEntryManagement:string;
 
 describe('login', () => {
-    it('logs in and accesses patient page', () => {
+    it('logs in and accesses medical record page', () => {
         cy.intercept('POST', '/api/auth/login').as('loginRequest');
         cy.visit("http://localhost:4200/login");
-        cy.get('#username').type('admin');
-        cy.get('#password').type('adminpassword');
+        cy.get('#username').type('tiago');
+        cy.get('#password').type('tiagopassword');
         cy.get('button[type="submit"]').click(); 
 
         cy.wait('@loginRequest').then((interception) => {
@@ -14,10 +14,7 @@ describe('login', () => {
             expect(tokenFamilyHistoryEntryManagement).to.exist;
         });
 
-        cy.url().should('include', '/admin');
-        
-        cy.contains('button', 'Gerir Pacientes').click();
-        cy.url().should('include', '/patientmanagement');
+        cy.url().should('include', '/doctor');
     })
 })
 describe('family history entry', () => {
@@ -30,12 +27,9 @@ describe('family history entry', () => {
     it('accesses medical record', () => {
         localStorage.setItem("token", tokenFamilyHistoryEntryManagement)
         cy.visit("http://localhost:4200/medicalrecord/202412000002");
+        cy.contains('Registo 202412000002');
 
-        cy.get('input[ng-reflect-name="FullNameSelected"]').should('exist').check();
-        cy.get('input[ng-reflect-name="FullNameValue"]').should('exist').type('Test One');
-
-        cy.get('button[type="submit"]').click();
-        cy.contains('Não foi encontrado nenhum Paciente com essas configurações.').should('be.visible');
+        cy.contains('Histórico Médico Familiar');
     });
 
     it('creates family history entry', () => {
@@ -43,53 +37,35 @@ describe('family history entry', () => {
 
         cy.visit("http://localhost:4200/medicalrecord/202412000002");
 
-        cy.contains('button', 'Criar um novo Paciente').click();
+        cy.contains('button', 'Histórico Médico Familiar').click();
+        cy.contains('button', 'Criar entrada').click();
 
-        cy.get('input[ng-reflect-name="FullName"]').type('Test One');
-        cy.get('input[ng-reflect-name="Email"]').type('diogo10072004@gmail.com');
-        cy.get('input[ng-reflect-name="PhoneNumber"]').type('999888777');
-        cy.get('input[ng-reflect-name="DateOfBirth"]').type('2004-10-07');
-        cy.get('select[ng-reflect-name="Gender"]').select('Male');
-
-        cy.contains('button', 'Adicionar Alergia').click();
-        cy.get('input[placeholder="Insira uma alergia"]').type('Gatos');
-        cy.get('input[placeholder="Insira uma alergia"]').should('have.value', 'Gatos'); 
+        cy.get('input[ng-reflect-name="relative"]').type('Mãe');
+        cy.get('input[ng-reflect-name="history"]').type('Osteoporose.');
 
         cy.contains('button', 'Salvar').click();
-        cy.contains(/Paciente \d+ criado com sucesso!/).invoke('text').then((text) => {
-            const match = text.match(/\d+/); // Procura o MedicalRecordNumber na string
-            if (match) {
-                MedicalRecordNumber = match[0];
-                console.log(MedicalRecordNumber);
-                cy.log(`Número do paciente: ${MedicalRecordNumber}`);
-                cy.wrap(MedicalRecordNumber).as('MedicalRecordNumber');
-            } else {
-                throw new Error('Não foi possível extrair o número do paciente.');
-            }
-        });
-
-        cy.get('@MedicalRecordNumber').then((MedicalRecordNumber) => {
-            cy.get('input[ng-reflect-name="MedicalRecordNumberSelected"]').should('exist').check();
-            cy.get('input[ng-reflect-name="MedicalRecordNumberValue"]').should('exist').type(String(MedicalRecordNumber));
-        });
-
-        cy.get('button[type="submit"]').click();
-        cy.contains('Test One'); //Found it!
+        cy.wait(1000);
     });
+
     it("edits family history entry's description", () => {
         localStorage.setItem("token", tokenFamilyHistoryEntryManagement);
         cy.visit("http://localhost:4200/medicalrecord/202412000002");
 
-        cy.get('input[ng-reflect-name="MedicalRecordNumberSelected"]').should('exist').check();
-        cy.get('input[ng-reflect-name="MedicalRecordNumberValue"]').should('exist').type(String(MedicalRecordNumber));
-        cy.get('button[type="submit"]').click();
-        cy.contains('Test One').click();
-        cy.contains('button', 'Editar').click();
+        cy.contains('button', 'Histórico Médico Familiar').click();
+        cy.contains('button', 'Pesquisar entradas').click();
 
-        cy.get('input[ng-reflect-name="EmailSelected"]').should('exist').check();
-        cy.get('input[ng-reflect-name="EmailValue"]').should('exist').clear().type('diogofscunha2004@gmail.com');
+        cy.get('input[ng-reflect-name="relativeSelected"]').should('exist').check();
+        cy.get('input[ng-reflect-name="relativeValue"]').should('exist').type("Mãe");
+        cy.get('input[ng-reflect-name="historySelected"]').should('exist').check();
+        cy.get('input[ng-reflect-name="historyValue"]').should('exist').type("Osteoporose.");
+        cy.get('button[type="submit"]').click();
+        
+        cy.contains('Editar').click(); //Yey, encontramos
+
+        cy.get('input[ng-reflect-name="historySelected"]').should('exist').check();
+        cy.get('input[ng-reflect-name="historyValue"]').should('exist').clear().type("Asma, Osteoporose.");
         cy.contains('button','Salvar').click();
-        cy.contains(`Paciente ${MedicalRecordNumber} editado com sucesso!`);
-        cy.contains('diogofscunha2004@gmail.com'); //It was successfully edited
+
+        cy.contains('Asma, Osteoporose.'); //It was successfully edited
     })
 });
